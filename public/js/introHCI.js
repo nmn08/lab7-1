708181cbf1a57e24116a5a4a42a0fee73608af38
Nhat Nguyen <nmn008@ucsd.edu>
2021-02-18 00:35:43 -0800

    Update lab 7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn(e) {
+	e.preventDefault();
+	ga('create', 'UA-190155764-1', 'auto');
+	ga('send', 'event', 'like', 'click');
 }
